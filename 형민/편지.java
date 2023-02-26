2bf30f91318cd26dec27be1eaf9d36a0ccf2acac
MINNIT <dr94406@naver.com>
2023-02-27 00:22:40 +0900

    feat: 프로그래머스 0레벨 1문제, 1레벨 5문제 추가
    스트링 배열, 문자열 비교 등
    기본적인 문법으로 푸는 문제 추가.

---
+++ 형민/편지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int sum = 0;
+        int width = 2;
+        for(int i = 0; i < message.length(); i++){
+            sum = message.length();
+            answer = sum * width;
+       
+        }
+        return answer;
+    }
+}
