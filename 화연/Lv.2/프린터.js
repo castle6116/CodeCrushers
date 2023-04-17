719be3d073abae0f5ef930e66a14a86cf9693b01
lazyhysong <37135523+eee0930@users.noreply.github.com>
2023-04-18 00:17:05 +0900

    feat: object 풀이 추가

--- 화연/Lv.2/프린터.js
+++ 화연/Lv.2/프린터.js
@@ -13,23 +13,87 @@ function solution(priorities, location) {
   let i = 0; // priorities 순서
   let idx = 0; // 대기목록 순서
   while(priorities.length > 0) {  
-      const value = priorities[i];
-      delete priorities[i];
-      if(value < priorArr[maxIdx]) {
-          priorities[PRI_SIZE + idx] = value;
-          if(i === location) {
-              location = PRI_SIZE + idx;
-          }
-          idx++;
-      } else {
-          maxIdx++;
-          if(i === location) {
-              answer = maxIdx;
-              break;
-          }
+    const value = priorities[i];
+    delete priorities[i];
+    if(value < priorArr[maxIdx]) {
+      priorities[PRI_SIZE + idx] = value;
+      if(i === location) {
+        location = PRI_SIZE + idx;
       }
-      i++;
+      idx++;
+    } else {
+      maxIdx++;
+      if(i === location) {
+        answer = maxIdx;
+        break;
+      }
+    }
+    i++;
   }
-  
   return answer;
 }
+
+
+
+/**
+ * 연결 리스트 Object를 이용한 풀이
+ */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  } 
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  
+  enqueue(newValue) {
+    const newNode = new Node(newValue);
+    if(this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+  
+  dequeue() {
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+  
+  peek() {
+    return this.head.value;
+  }
+}
+
+function solution(priorities, location) {
+  const queue = new Queue();
+  for(let i = 0; i < priorities.length; i++) {
+    queue.enqueue([priorities[i], i]);
+  }
+  
+  priorities.sort((a, b) => b - a);
+  
+  let count = 0;
+  while(true) {
+    const currentValue = queue.peek();
+    if(currentValue[0] < priorities[count]) {
+      queue.enqueue(queue.dequeue());
+    } else {
+      const value = queue.dequque();
+      count += 1;
+      if(location === value[1]) {
+        return count;
+      }
+    }
+  }
+  
+  return count;
+}
+
