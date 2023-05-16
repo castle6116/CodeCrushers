2631356566ceaf6970c3ecc0bcef58dce788c5ca
MINNIT <dr94406@naver.com>
2023-05-16 20:18:08 +0900

    feat: 프로그래머스 0레벨 3문제 추가

---
+++ 형민/Lv.0/뒤에서 5등까지.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = num_list;
+        Arrays.sort(num_list);
+        return  answer = Arrays.copyOfRange(answer, 0, 5);
+    }
+}
