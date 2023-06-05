ed9742414ddabafcd65935bb89a181de267446d3
youredith <youredith3@gmail.com>
2023-06-05 15:00:13 +0900

    feat: 프로그래머스 Lv2 2문제 추가

---
+++ 지은/Lv.2/124나라의숫자.py
@@ -0,0 +1,20 @@
+def solution(n):
+    if n <= 3:
+        return '124'[n-1]
+    else:
+        q, r = divmod(n-1, 3)
+        return solution(q) + '124'[r]
+
+# 다른 사람의 풀이
+
+
+def change124(n):
+    num = ['1', '2', '4']
+    answer = ""
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
