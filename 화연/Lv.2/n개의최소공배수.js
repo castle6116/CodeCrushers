e442933d3a24965faf9d016e342e52c65b5d9b37
lazyhysong <eee0930@naver.com>
2023-09-01 15:42:28 +0900

    feat: 프로그래머스 lv.2 1문제 추가 및 일정추가

--- 화연/Lv.2/n개의최소공배수.js
+++ 화연/Lv.2/n개의최소공배수.js
@@ -1,8 +1,8 @@
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/12953
- * 
+ *
  * GCD = 최대공약수, LCM = 최소공배수, 두 수 (a, b) => a x b = GCD * LCM
- * 
+ *
  * [유클리드 호제법]
  * (13, 17)의 최대공약수는?
  * 17 % 13 = 4
@@ -10,33 +10,33 @@
  * 4 % 1 = 0이므로
  * 최대공약수는 1
  * @author HwaYeon
- * @param {*} arr 
- * @returns 
+ * @param {*} arr
+ * @returns
  */
 function solution(arr) {
   let answer = 0;
   const getSlice = (multi, m, n) => {
     let big = m;
     let small = n;
-    if(m%n === 0) {
-      return multi/small;
+    if (m % n === 0) {
+      return multi / small;
     } else {
       big = n;
-      small = m%n;
+      small = m % n;
       return getSlice(multi, big, small);
     }
-  }
+  };
   const getLeast = (n1, n2) => {
-    if(n1 === n2) {
+    if (n1 === n2) {
       return n1;
-    } else if(n1 > n2) {
-      return getSlice(n1*n2, n1, n2);
+    } else if (n1 > n2) {
+      return getSlice(n1 * n2, n1, n2);
     } else {
-      return getSlice(n1*n2, n2, n1);
+      return getSlice(n1 * n2, n2, n1);
     }
-  }
-  for(let i = 1; i < arr.length; i++) {
-    if(i === 1) {
+  };
+  for (let i = 1; i < arr.length; i++) {
+    if (i === 1) {
       answer = getLeast(arr[0], arr[1]);
     } else {
       answer = getLeast(answer, arr[i]);
